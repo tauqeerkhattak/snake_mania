a9c33632ee035ad2f2b21045c1bee5199afd5d5e
tauqeerkhattak <tauqeer745@outlook.com>
2023-11-03 13:31:54 +0100

    in progress

--- lib/ui/components/snake.dart
+++ lib/ui/components/snake.dart
@@ -1,195 +1,268 @@
-import 'dart:developer';
-import 'dart:math' hide log;
+// import 'dart:developer';
+// import 'dart:math' hide log;
+//
+// import 'package:flame/collisions.dart';
+// import 'package:flame/components.dart';
+// import 'package:flame/sprite.dart';
+// import 'package:flutter/services.dart';
+// import 'package:snake_mania/ui/snake_game.dart';
+// import 'package:snake_mania/utils/resources.dart';
+// import 'package:snake_mania/utils/vector2_utils.dart';
+//
+// import '../../models/snake_position.dart';
+//
+// class Snake extends PositionComponent
+//     with HasGameRef<SnakeGame>, CollisionCallbacks, KeyboardHandler {
+//   final speed = 180;
+//   bool hasCollided = false;
+//   final VoidCallback onCollide;
+//   List<SnakePosition> snakeParts = [];
+//   SnakeDirection direction = SnakeDirection.right;
+//
+//   Snake({
+//     required this.onCollide,
+//   });
+//
+//   @override
+//   Future<void> onLoad() async {
+//     await super.onLoad();
+//     final image = game.images.fromCache('sheet.png');
+//     final sheet = SpriteSheet(
+//       image: image,
+//       srcSize: Vector2(snakeSpriteSize, snakeSpriteSize),
+//     );
+//     final head1 = sheet.getSprite(0, 0);
+//     // final head2 = sheet.getSprite(1, 0);
+//     // final head3 = sheet.getSprite(2, 0);
+//     // animation = SpriteAnimation.spriteList(
+//     //   [
+//     //     head3,
+//     //     head2,
+//     //     head1,
+//     //     head2,
+//     //     head3,
+//     //   ],
+//     //   stepTime: 0.25,
+//     // );
+//     final initialPosition = Vector2(0, 0);
+//     snakeParts.add(
+//       SnakePosition(
+//         direction: SnakeDirection.right,
+//         position: initialPosition,
+//         bodyPart: head1,
+//         bodyType: SnakeBodyType.head,
+//       ),
+//     );
+//     snakeParts.add(
+//       SnakePosition(
+//         direction: SnakeDirection.right,
+//         position: initialPosition.minusX(snakeSize),
+//         bodyPart: sheet.getSprite(2, 2),
+//         bodyType: SnakeBodyType.body,
+//       ),
+//     );
+//     snakeParts.add(
+//       SnakePosition(
+//         direction: SnakeDirection.right,
+//         position: initialPosition.minusX(snakeSize * 2),
+//         bodyPart: sheet.getSprite(2, 1),
+//         bodyType: SnakeBodyType.tail,
+//       ),
+//     );
+//     for (final part in snakeParts) {
+//       final component = SpriteComponent(
+//         sprite: part.bodyPart,
+//         position: part.position,
+//         angle: -pi / 2,
+//         size: Vector2(snakeSize, snakeSize),
+//         anchor: Anchor.center,
+//       );
+//       if (part.isTail) {
+//         component.flipVerticallyAroundCenter();
+//       }
+//       add(
+//         component,
+//       );
+//     }
+//     // add(
+//     //   SpriteComponent(
+//     //     sprite: sheet.getSprite(2, 2),
+//     //     position: Vector2(0, -snakeSize),
+//     //     size: Vector2(snakeSize, snakeSize),
+//     //   ),
+//     // );
+//     // add(
+//     //   SpriteComponent(
+//     //     sprite: sheet.getSprite(2, 1),
+//     //     position: Vector2(snakeSize, -snakeSize),
+//     //     angle: pi,
+//     //     size: Vector2(snakeSize, snakeSize),
+//     //   ),
+//     // );
+//     // angle = -pi / 2;
+//     add(RectangleHitbox());
+//   }
+//
+//   int counter = -10;
+//
+//   @override
+//   void update(double dt) {
+//     super.update(dt);
+//     if (!hasCollided) {
+//       // switch (direction) {
+//       //   case SnakeDirection.up:
+//       //     position = Vector2(
+//       //       position.x,
+//       //       position.y - (dt * speed),
+//       //     );
+//       //     break;
+//       //   case SnakeDirection.down:
+//       //     position = Vector2(
+//       //       position.x,
+//       //       position.y + (dt * speed),
+//       //     );
+//       //     break;
+//       //   case SnakeDirection.left:
+//       //     position = Vector2(
+//       //       position.x - (dt * speed),
+//       //       position.y,
+//       //     );
+//       //     break;
+//       //   case SnakeDirection.right:
+//       //     position = Vector2(
+//       //       position.x + (dt * speed),
+//       //       position.y,
+//       //     );
+//       //     break;
+//       // }
+//
+//       if (children.isNotEmpty) {
+//         if (counter >= children.length || counter.isNegative) {
+//           counter = -10;
+//           return;
+//         }
+//         final component = children.toList()[counter];
+//         if (component is PositionComponent) {
+//           changeComponentAngle(component);
+//
+//         }
+//         // for (int i = 0; i < children.length; i++) {
+//         //   // if (i == children.length) {
+//         //   //   counter = -10;
+//         //   //   return;
+//         //   // } else if (counter.isNegative) {
+//         //   //   return;
+//         //   // }
+//         //
+//         // }
+//       }
+//     }
+//   }
+//
+//   void changeComponentAngle(PositionComponent component) {
+//     switch (direction) {
+//       case SnakeDirection.up:
+//         component.angle = pi;
+//       case SnakeDirection.down:
+//         component.angle = 0;
+//       case SnakeDirection.left:
+//         component.angle = pi / 2;
+//       case SnakeDirection.right:
+//         component.angle = -pi / 2;
+//     }
+//   }
+//
+//   void changeComponentPosition(PositionComponent component, double dt) {
+//     switch (direction) {
+//       case SnakeDirection.up:
+//         component.position = component.position.minusY(dt * speed);
+//
+//       case SnakeDirection.down:
+//         component.position = component.position.minusY(dt * -speed);
+//       case SnakeDirection.left:
+//         component.position = component.position.minusX(dt * speed);
+//       case SnakeDirection.right:
+//         component.position = component.position.minusX(dt * -speed);
+//     }
+//   }
+//
+//   @override
+//   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+//     super.onCollision(intersectionPoints, other);
+//     if (other is ScreenHitbox) {
+//       if (!hasCollided) {
+//         log('Game should stop here!');
+//         hasCollided = true;
+//         onCollide();
+//         // animation = null;
+//       }
+//     }
+//   }
+//
+//   @override
+//   bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+//     super.onKeyEvent(event, keysPressed);
+//     if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
+//       log('DOWN');
+//       // if (children.isNotEmpty) {
+//       //   final head = children.first as SpriteComponent;
+//       //   head.angle = 0;
+//       // }
+//       direction = SnakeDirection.down;
+//       counter = 0;
+//     } else if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
+//       log('UP');
+//       direction = SnakeDirection.up;
+//       counter = 0;
+//       // if (children.isNotEmpty) {
+//       //   final head = children.first as SpriteComponent;
+//       //   head.angle = pi;
+//       // }
+//     } else if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+//       log('LEFT');
+//       direction = SnakeDirection.left;
+//       counter = 0;
+//       // if (children.isNotEmpty) {
+//       //   final head = children.first as SpriteComponent;
+//       //   head.angle = pi / 2;
+//       // }
+//     } else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+//       log('RIGHT');
+//       direction = SnakeDirection.right;
+//       counter = 0;
+//       // if (children.isNotEmpty) {
+//       //   final head = children.first as SpriteComponent;
+//       //   head.angle = -pi / 2;
+//       // }
+//     }
+//     return true;
+//   }
+// }
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame/sprite.dart';
-import 'package:flutter/services.dart';
-import 'package:snake_mania/ui/snake_game.dart';
-import 'package:snake_mania/utils/resources.dart';
-import 'package:snake_mania/utils/vector2_utils.dart';
-
-import '../../models/snake_position.dart';
+import 'package:flutter/cupertino.dart';
 
 class Snake extends PositionComponent
-    with HasGameRef<SnakeGame>, CollisionCallbacks, KeyboardHandler {
-  final speed = 100;
-  bool hasCollided = false;
-  final VoidCallback onCollide;
-  List<SnakePosition> snakeParts = [];
-  SnakeDirection direction = SnakeDirection.right;
+    with HasCollisionDetection, CollisionCallbacks {
+  final VoidCallback? onCollide;
+  final Sprite sprite;
 
   Snake({
-    required this.onCollide,
+    this.onCollide,
+    required this.sprite,
   });
 
   @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-    final image = game.images.fromCache('sheet.png');
-    final sheet = SpriteSheet(
-      image: image,
-      srcSize: Vector2(snakeSpriteSize, snakeSpriteSize),
-    );
-    final head1 = sheet.getSprite(0, 0);
-    // final head2 = sheet.getSprite(1, 0);
-    // final head3 = sheet.getSprite(2, 0);
-    // animation = SpriteAnimation.spriteList(
-    //   [
-    //     head3,
-    //     head2,
-    //     head1,
-    //     head2,
-    //     head3,
-    //   ],
-    //   stepTime: 0.25,
-    // );
-    final initialPosition = Vector2(0, 0);
-    snakeParts.add(
-      SnakePosition(
-        direction: SnakeDirection.right,
-        position: initialPosition,
-        bodyPart: head1,
-        bodyType: SnakeBodyType.head,
-      ),
-    );
-    snakeParts.add(
-      SnakePosition(
-        direction: SnakeDirection.right,
-        position: initialPosition.minusX(snakeSize),
-        bodyPart: sheet.getSprite(2, 2),
-        bodyType: SnakeBodyType.body,
-      ),
-    );
-    snakeParts.add(
-      SnakePosition(
-        direction: SnakeDirection.right,
-        position: initialPosition.minusX(snakeSize * 2),
-        bodyPart: sheet.getSprite(2, 1),
-        bodyType: SnakeBodyType.tail,
-      ),
-    );
-    for (final part in snakeParts) {
-      final component = SpriteComponent(
-        sprite: part.bodyPart,
-        position: part.position,
-        angle: -pi / 2,
-        size: Vector2(snakeSize, snakeSize),
-        anchor: Anchor.center,
-      );
-      if (part.isTail) {
-        component.flipVerticallyAroundCenter();
-      }
-      add(
-        component,
-      );
-    }
-    // add(
-    //   SpriteComponent(
-    //     sprite: sheet.getSprite(2, 2),
-    //     position: Vector2(0, -snakeSize),
-    //     size: Vector2(snakeSize, snakeSize),
-    //   ),
-    // );
-    // add(
-    //   SpriteComponent(
-    //     sprite: sheet.getSprite(2, 1),
-    //     position: Vector2(snakeSize, -snakeSize),
-    //     angle: pi,
-    //     size: Vector2(snakeSize, snakeSize),
-    //   ),
-    // );
-    // angle = -pi / 2;
-    add(RectangleHitbox());
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    if (!hasCollided) {
-      switch (direction) {
-        case SnakeDirection.up:
-          position = Vector2(
-            position.x,
-            position.y - (dt * speed),
-          );
-          break;
-        case SnakeDirection.down:
-          position = Vector2(
-            position.x,
-            position.y + (dt * speed),
-          );
-          break;
-        case SnakeDirection.left:
-          position = Vector2(
-            position.x - (dt * speed),
-            position.y,
-          );
-          break;
-        case SnakeDirection.right:
-          position = Vector2(
-            position.x + (dt * speed),
-            position.y,
-          );
-          break;
-      }
-
-      // if (children.isNotEmpty) {
-      //   final head = children.first;
-      //   for (final component in children) {
-      //     if (component is SpriteComponent) {
-      //
-      //     }
-      //   }
-      // }
-    }
+  void render(Canvas canvas) {
+    super.render(canvas);
+    sprite.render(canvas);
+    anchor = Anchor.center;
   }
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollision(intersectionPoints, other);
-    if (other is ScreenHitbox) {
-      if (!hasCollided) {
-        log('Game should stop here!');
-        hasCollided = true;
-        onCollide();
-        // animation = null;
-      }
-    }
-  }
-
-  @override
-  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
-    super.onKeyEvent(event, keysPressed);
-    if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
-      log('DOWN');
-      if (children.isNotEmpty) {
-        final head = children.first as SpriteComponent;
-        head.angle = 0;
-      }
-      direction = SnakeDirection.down;
-    } else if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
-      log('UP');
-      direction = SnakeDirection.up;
-      if (children.isNotEmpty) {
-        final head = children.first as SpriteComponent;
-        head.angle = pi;
-      }
-    } else if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
-      log('LEFT');
-      direction = SnakeDirection.left;
-      if (children.isNotEmpty) {
-        final head = children.first as SpriteComponent;
-        head.angle = pi / 2;
-      }
-    } else if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
-      log('RIGHT');
-      direction = SnakeDirection.right;
-      if (children.isNotEmpty) {
-        final head = children.first as SpriteComponent;
-        head.angle = -pi / 2;
-      }
-    }
-    return true;
+    onCollide?.call();
   }
 }
--- lib/ui/snake_game.dart
+++ lib/ui/snake_game.dart
@@ -1,13 +1,29 @@
+import 'dart:developer' as dev;
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart' hide Viewport;
+import 'package:flutter/services.dart';
 import 'package:snake_mania/ui/components/game_over_component.dart';
 import 'package:snake_mania/ui/components/snake.dart';
 import 'package:snake_mania/utils/resources.dart';
+import 'package:snake_mania/utils/vector2_utils.dart';
 
 class SnakeGame extends FlameGame
-    with HasCollisionDetection, HasKeyboardHandlerComponents {
-  late Vector2 position;
+    with HasCollisionDetection, HasKeyboardHandlerComponents, HasGameRef {
+  late Snake snakeHead;
+  late Snake snakeBody;
+  late Snake snakeTail;
+
+  // SnakeDirection headDirection = SnakeDirection.down;
+  // SnakeDirection bodyDirection = SnakeDirection.down;
+  // SnakeDirection tailDirection = SnakeDirection.down;
+  //
+  // int counter = -10;
+  final speed = 50;
 
   void _onCollision() {
     add(GameOverComponent());
@@ -16,18 +32,74 @@ class SnakeGame extends FlameGame
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    await images.loadAll([
-      'sheet.png',
-    ]);
-    position = size / 2;
-    final snake = Snake(
-      onCollide: _onCollision,
+    final position = size / 2;
+    final image = await images.load('sheet.png');
+    final sheet = SpriteSheet(
+      image: image,
+      srcSize: Vector2(snakeSpriteSize, snakeSpriteSize),
     );
-    snake.position = position;
-    snake.size = Vector2(snakeSize, snakeSize);
-    add(snake);
+    final snakeBodySize = Vector2(snakeSize, snakeSize);
+    snakeHead = Snake(
+      onCollide: _onCollision,
+      sprite: sheet.getSprite(0, 0),
+    );
+    snakeBody = Snake(sprite: sheet.getSprite(2, 2));
+    snakeTail = Snake(sprite: sheet.getSprite(2, 1));
+
+    snakeHead.position = position;
+    snakeBody.position = position.minusY(snakeSize);
+    snakeTail.position = position.minusY(snakeSize + snakeSize);
+
+    snakeHead.size = snakeBodySize;
+    snakeBody.size = snakeBodySize;
+    snakeTail.size = snakeBodySize;
+    snakeTail.flipVerticallyAroundCenter();
+
+    add(snakeHead);
+    add(snakeBody);
+    add(snakeTail);
     add(
       ScreenHitbox(),
     );
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    changeSnakePosition(dt);
+  }
+
+  void changeSnakePosition(double dt) {
+    final newSpeed = -dt * speed;
+    final newPosition = snakeHead.position.minusY(newSpeed);
+    snakeHead.position = newPosition;
+    snakeBody.position = snakeBody.position.minusY(newSpeed);
+    snakeTail.position = snakeTail.position.minusY(newSpeed);
+
+    // if (counter == 0) {
+    //   bodyDirection = headDirection
+    // }
+    //
+    // if (counter > 2) {
+    //   counter = -10;
+    // }
+    // else {
+    //   counter++;
+    // }
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+      dev.log('hehe');
+    } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+    } else if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+      dev.log('Arrow Left');
+      camera.viewfinder.angle = -pi;
+    } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {}
+    return super.onKeyEvent(event, keysPressed);
   }
 }
